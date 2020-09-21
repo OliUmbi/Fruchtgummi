ec4fd3b4553d5bd6684d68e72a9e5fa33379babf
OliUmbi <65683302+OliUmbi@users.noreply.github.com>
2020-09-21 20:45:37 +0200

    Works now in Python

--- castle.py
+++ castle.py
@@ -1,37 +1,50 @@
+from multiprocessing import Pool
 from random import randint
-from numpy import all
+from numpy import matrix
 
+iterations = 1000
 combinations = []
 
-for pathLenght in range(2, 11):
-    for sweets in range(6, 31):
+for pathLenght in range(6, 31):
+    for sweets in range(2, 11):
         combinations.append([pathLenght, sweets])
 
-print(combinations)
-
-won = []
-
+data = []
 
 def runiterations(path, sweet):
-    global won
-    for x in range(100):
+    global data
+    win = 0
+    avgPlayes = []
+    
+    for x in range(iterations):
+        playes = 0
         position = 0
-        castle = {1: sweet, 2: sweet, 3: sweet, 4: sweet, 5: sweet}
+        castle = [sweet, sweet, sweet, sweet, sweet]
         while True:
-            rd = randint(1, 6)
-            if rd == 6:
+            playes = playes + 1
+            rd = randint(0, 5)
+            if rd == 5:
                 position = position + 1
             else:
                 castle[rd] = castle[rd] - 1
             if position == path:
-                won.append("ghost")
                 break
-            print(castle)
-            if all(castle == 0):
-                won.append("player")
+            if max(castle) <= 0:
+                win = win + 1
                 break
+        avgPlayes.append(playes)
+    
+    winRate = 100 / iterations * win
+    avg = sum(avgPlayes) / len(avgPlayes)
+    data.append([path, sweet, winRate, avg])
 
 
-runiterations(10, 2)
+#pool = Pool()
+#pool.map(runiterations, combinations)
 
-print(won)
+for combination in combinations:
+    runiterations(combination[0], combination[1])
+
+data = sorted(data, key=lambda x: x[2], reverse=True)
+
+print(matrix(data))
